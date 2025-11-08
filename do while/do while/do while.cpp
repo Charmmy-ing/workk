b7f651709eab4a4adb7bcb6cd4330d161d3a52cb
Charmmmy <2127137114@qq.com>
2025-11-08 22:04:29 +0800

    Add files via upload

---
+++ do while/do while/do while.cpp
@@ -0,0 +1,11 @@
+﻿#include<stdio.h>
+int main()
+{
+	int A = 3;
+	do
+	{
+		printf("%d\n", A);
+		A++;
+     }while (A>3 );
+	return 0;
+}
---
+++ while 印数字/while 印数字/while 印数字.cpp
@@ -0,0 +1,11 @@
+﻿#include<stdio.h>
+int main()
+{
+	int F = 1;
+	while (F <= 10)
+	{
+		printf("%d\n", F);
+		F = F + 1;
+	}
+	return 0;
+}
